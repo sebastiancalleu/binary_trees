26807b8f4b65ab5d05df734cab84349ed14855a3
sebastian <sebastian.calleu@gmail.com>
2021-03-10 15:47:56 -0500

    adding leaf-check prototype

--- binary_trees.h
+++ binary_trees.h
@@ -25,5 +25,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *, int);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *, int);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *, int);
 void binary_tree_delete(binary_tree_t *);
+int binary_tree_is_leaf(const binary_tree_t *node);
 
 #endif /* _BINARY_TREES_H_ */

e4ae8e0c85712d3317eee4d4c1c44a27c782fb06
Giraluna1 <lunagolo@hotmail.com>
2021-03-10 15:11:58 -0500

    Add control if parent is null

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *nodeleft;
 
 	nodeleft = malloc(sizeof(binary_tree_t));
-	if (nodeleft == NULL)
+	if (nodeleft == NULL | parent == NULL)
 		return (NULL);
 	nodeleft->n = value;
 	nodeleft->parent = parent;

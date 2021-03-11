be24588c8f08a4855375c2806410642beb769e5d
Giraluna1 <lunagolo@hotmail.com>
2021-03-10 19:14:14 -0500

    Fixed betty description

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,5 +1,10 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_depth - This function measure the depth of the tree
+ * @tree: pointer to the root node
+ * Return: the measure
+ */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t counter;

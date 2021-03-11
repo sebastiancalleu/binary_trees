6c3306f04e4a40917a8070c4dc292d4d70599596
Giraluna1 <lunagolo@hotmail.com>
2021-03-10 19:15:54 -0500

    Add control if tree is null

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,7 +9,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t counter;
 
-	if (tree->parent == NULL)
+	if (tree->parent == NULL || tree == NULL)
 		return (0);
 	for (counter = 0; tree->parent != NULL; counter++)
 		tree = tree->parent;
